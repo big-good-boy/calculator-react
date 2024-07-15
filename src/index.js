fc4d23912658fa4fc8959616775dfc5063d3a077
big-good-boy <good_boy@live.ru>
2024-07-15 20:11:40 +0500

    start project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './calculator.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+class Calculator extends React.Component {
+	render() {
+		return (
+			<div className="calculator">
+				<div className="calculator__inner">
+					<div className="calculator__result">24</div>
+					<div className="calculator__buttons">
+						<button className="calculator__button">C</button>
+						<button className="calculator__button">%</button>
+						<button className="calculator__button">√</button>
+						<button className="calculator__button">÷</button>
+						<button className="calculator__button">7</button>
+						<button className="calculator__button">8</button>
+						<button className="calculator__button">9</button>
+						<button className="calculator__button">✕</button>
+						<button className="calculator__button">4</button>
+						<button className="calculator__button">5</button>
+						<button className="calculator__button">6</button>
+						<button className="calculator__button">-</button>
+						<button className="calculator__button">1</button>
+						<button className="calculator__button">2</button>
+						<button className="calculator__button">3</button>
+						<button className="calculator__button">+</button>
+						<button className="calculator__button">0</button>
+						<button className="calculator__button">.</button>
+						<button className="calculator__button">±</button>
+						<button className="calculator__button">=</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Calculator />
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
